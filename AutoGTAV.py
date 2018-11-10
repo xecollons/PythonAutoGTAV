79f9fe263a399036b88f6da631e41c45eb0be4b6
xecollons <x.collons@gmail.com>
2018-11-10 02:46:58 +0100

    first commit

--- AutoGTAV.py
+++ AutoGTAV.py
@@ -3,14 +3,23 @@ from PIL import ImageGrab
 import cv2
 import time
 
+def process_img(original_image):
+    #We convert to gray
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    #edge detection
+    processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    return processed_img
+
 def screen_record():
     last_time = time.time()
     while(True):
         #800x600 windowed mode
-        printscreen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        screen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        new_screen = process_img(screen)
+        cv2.imshow('window',new_screen)
+        #cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
