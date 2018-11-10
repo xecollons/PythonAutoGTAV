bd516ec246da2452fbe2eb1d6bbeced26ac20fdf
xecollons <x.collons@gmail.com>
2018-11-10 03:04:18 +0100

    First iteration of autokeys

--- AutoGTAV.py
+++ AutoGTAV.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+from directkeys import PressKey, ReleaseKey, W, S, D, A
 
 def process_img(original_image):
     #We convert to gray
@@ -15,9 +16,14 @@ def screen_record():
     while(True):
         #800x600 windowed mode
         screen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        PressKey(W)
+        time.sleep(3)
+        PressKey(W)
+        time.sleep(3)
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         new_screen = process_img(screen)
+        #now the im updates ocurr every 6 seconds because of the sleeps
         cv2.imshow('window',new_screen)
         #cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
